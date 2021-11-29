3d1388097410e6bead9ca93413e34f8403ee605f
ChenYang Lin <chenyanglinq@gmail.com>
2021-11-29 12:12:42 -0500

    sprint4 datetime

--- server.js
+++ server.js
@@ -269,7 +269,7 @@ app.post("/admin/manuallyAddEvent", checkAuthenticated, upload.single('inputImag
   let manually = new Manually({
     title: inputTitle,
     image: inputImage,
-    dateTime: dateObject.toLocaleString('en-US', { timeZone: 'America/New_York', dateStyle: "full", timeStyle: "medium" }),
+    dateTime: dateObject.toLocaleString('en-US', { timeZone: 'America/New_York', dateStyle: "full", timeStyle: "short" }),
     linkToOriginalPost: inputLink,
     detailDateTime: dateObject.toString(),
     address: inputAddress,
@@ -398,7 +398,7 @@ app.post("/requestEvent", upload.single('inputImage'), async (req, res) => {
   // dateObject = new Date(date[0], date[1] - 1, date[2], time[0], time[1], 0, 0);
   dateObject = new Date(Date.UTC(date[0], date[1] - 1, date[2], time[0], time[1], 0, 0));
     dateObject = new Date(dateObject.getTime() + offset*60*1000);
-  let dateString = dateObject.toLocaleString('en-US', { timeZone: 'America/New_York', dateStyle: "full", timeStyle: "medium" })
+  let dateString = dateObject.toLocaleString('en-US', { timeZone: 'America/New_York', dateStyle: "full", timeStyle: "long" })
   dateString = dateString.toLowerCase();
   if (dateString.includes("edt")) 
     dateObject.setHours( dateObject.getHours() - 1 );
